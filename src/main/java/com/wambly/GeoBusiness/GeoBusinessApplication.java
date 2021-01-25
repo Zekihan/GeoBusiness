1adc65b12c734a85a99455856c46dd2c9ce419e2
Zekihan AZMAN <zekihanazman99@gmail.com>
2021-01-25 23:05:53 +0300

    init

---
+++ src/main/java/com/wambly/GeoBusiness/GeoBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.wambly.GeoBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wambly/GeoBusiness/GeoBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wambly.GeoBusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
